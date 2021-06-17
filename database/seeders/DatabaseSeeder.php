2cb7d472376092dc3f37430e5da8161e20e0cecd
AyatANSSAIEN <ayat.anssaien@etu.uae.ac.ma>
2021-06-17 14:23:30 +0100

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Feedback;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        $this->call([
+            userSeeder::class,
+            annoncesSeeder::class, 
+            feedbackArticlesSeeder::class, 
+            feedbackClientsSeeder::class,
+            annoncedispoSeeder::class,
+        ]);
+    }
+}
---
+++ database/seeders/userSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Str;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+
+class userSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach(range(1,5) as $value){
+            DB::table('users')->insert([
+                'name' => $faker->unique()->username,
+                'email' => $faker->unique()->email,
+                'job' => $faker->jobTitle,
+                'description' => $faker->text,
+                'country' => $faker->country,
+                'city' => $faker->city,
+                'password' => hash::make("oussama123"),
+                'profile_photo_path' => "/profile-photos/".$faker->image('public/storage/profile-photos/',640,640,'people', false),
+                'created_at' => $faker->dateTime,
+                'role' => $faker->passthrough(mt_rand(1, 2)),
+            ]);
+        }
+    }
+}
